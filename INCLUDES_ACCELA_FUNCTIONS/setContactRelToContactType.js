fc909b0e18387bddf8b8c0430d80e63d8ae9d742
jchalk771 <jchalk@accela.com>
2017-08-28 13:41:20 -0400

    Adding function called by all BPT base solutions on ASA/CTRCA/CEA/CAA

---
+++ INCLUDES_ACCELA_FUNCTIONS/setContactRelToContactType.js
@@ -0,0 +1,17 @@
+function setContactRelToContactType() {
+	try {
+
+		iCont = null;
+		contactArray = new Array();
+		contactArray = getContactArray();
+		if (contactArray.length > 0) {
+			for (iCont in contactArray) {
+				tContact = contactArray[iCont];
+				aa.print("ContactName: " + tContact["firstName"] + " " + tContact["lastName"] + " " + tContact["contactType"]);
+				contactSetRelation(tContact["contactSeqNumber"], tContact["contactType"]);
+			}
+		}
+	} catch (err) {
+		logDebug("A JavaScript Error occured in setContactRelToContactType function: " + err.message + " In Line " + err.lineNumber);
+	}
+}
