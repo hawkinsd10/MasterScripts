f3edf927595709120e72d67adfc2df820b3cfaf5
David Bischof <dbischof@accela.com>
2017-10-24 09:54:00 -0400

    Adding STANDARD_SOLUTIONS and CONFIGURABLE_RULESETS to master scripts

--- MasterScripts/ExaminationUpdateAfter.js
+++ MasterScripts/ExaminationUpdateAfter.js
@@ -60,7 +60,19 @@ if (SA) {
 	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useCustomScriptFile));
 }
 
-eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
+eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));  
+ 
+ //Add Standard Solution Includes 
+ solutionInc = aa.bizDomain.getBizDomain("STANDARD_SOLUTIONS").getOutput().toArray(); 
+ for (sol in solutionInc) { 
+       if (solutionInc[sol].getAuditStatus() != "I") eval(getScriptText(solutionInc[sol].getBizdomainValue(),null)); 
+ }  
+ 
+ //Add Configurable RuleSets 
+ configRules = aa.bizDomain.getBizDomain("CONFIGURABLE_RULESETS").getOutput().toArray(); 
+ for (rule in configRules) { 
+       if (configRules[rule].getAuditStatus() != "I") eval(getScriptText(configRules[rule].getBizdomainValue(),null)); 
+ }
 
 if (documentOnly) {
 	doStandardChoiceActions(controlString, false, 0);
